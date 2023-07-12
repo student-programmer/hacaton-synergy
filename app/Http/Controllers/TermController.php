e7f19a4ccc6611f2ff22a613110bb11b0641d437
Alex Agafonov <awa77@mail.ru>
2023-07-12 14:26:45 +0300

    Add models, migrations, controllers and other (autogenerated)

---
+++ app/Http/Controllers/TermController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTermRequest;
+use App\Http\Requests\UpdateTermRequest;
+use App\Models\Term;
+
+class TermController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTermRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Term $term)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Term $term)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTermRequest $request, Term $term)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Term $term)
+    {
+        //
+    }
+}
